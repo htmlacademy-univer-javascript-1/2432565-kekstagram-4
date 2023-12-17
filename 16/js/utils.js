12415d607d531e719cbf41496670f73e8700919c
Keks <keks@htmlacademy.ru>
2023-12-17 10:25:26 +0300

    :heavy_check_mark: Сборка #16

---
+++ 16/js/utils.js
@@ -0,0 +1,13 @@
+const isEscKey = (evt) => evt.key === 'Escape';
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+
+export {isEscKey, debounce, shuffle};
